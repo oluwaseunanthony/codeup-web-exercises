ac21d2de974186eeb5a59817bc8f6da96c55081c
oluwaseun anthony <oluwaseun.a.anthony@gmail.com>
2023-10-11 15:00:05 -0500

    added a new operator file with class assignment in it

---
+++ Operators.js
@@ -0,0 +1,77 @@
+'1' + 2
+// 12
+console.log('1' + 2)
+
+"" ** 4
+//0
+console.log("" ** 4)
+
+typeof '1' + 2
+ //
+ console.log(typeof '1' + 2)
+
+6 % 4
+//2
+console.log(6 % 4)
+
+
+'3 + 4 is ' + 3 + 4
+// how could you make this produce the correct output?
+//
+console.log('3 + 4 is ' + 3 + 4)
+
+0 < 0
+//false
+console.log(0 < 0)
+
+'false' == false
+// false
+console.log('false' == false)
+
+true == 'true'
+//  false
+console.log(true == 'true')
+
+5 >= -5
+// true
+console.log(5 >= -5)
+
+!false || false
+//true
+console.log(!false || false)
+
+true || "42"
+//true
+console.log(true || "42")
+
+!true && !false
+//false
+console.log(!true && !false)
+
+6 % 5
+//1
+console.log(6 % 5)
+
+5 < 4 && 1 === 1
+//false
+console.log(5 < 4 && 1 === 1)
+
+typeof 'codeup' === 'string'
+//true
+console.log(typeof 'codeup' === 'string')
+
+'codeup' === 'codeup' && 'codeup' === 'Codeup'
+//false
+console.log('codeup' === 'codeup' && 'codeup' === 'Codeup')
+
+4 >= 0 && 1 !== '1'
+//true
+console.log(4 >= 0 && 1 !== '1')
+
+6 % 3 === 0
+//true
+console.log(6 % 3 === 0)
+
+5 % 2 !== 0
+//true
+console.log(5 % 2 !== 0)
