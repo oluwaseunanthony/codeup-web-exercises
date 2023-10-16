7694a17fb4e120cfb5fa79aa8eadad0a821d62ef
oluwaseun anthony <oluwaseun.a.anthony@gmail.com>
2023-10-16 12:02:13 -0500

    updated condition.js file with more class examples

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,18 +1,84 @@
-let age = 12;
-if (age>= 16){
-    console.log("You're eligible to drive")
-}
+// let age = 12;
+// if (age>= 16){
+//     console.log("You're eligible to drive")
+// }
+//
+// if (age<16){
+//     console.log("You can not drive")
+// }
+// // same example with else statement
+//
+// if (age>= 16){
+//     console.log("You can drive")
+// } else {
+//     console.log("grow up")
+// }
+//
+// let currentDay=prompt("Whats the day today?").toLowerCase()
+//
+// if(currentDay==='monday'|| currentDay==='tuesday'||currentDay==='wednesday'){
+//     console.log('I love '+ currentDay)
+// } else {
+//     console.log(currentDay + ' is not a valid day')
+// }
+//
+// let letsPlay= confirm(' Do you want to play')
+//
+// if (letsPlay){
+//     console.log("Great lets play xyz")
+// } else {
+//     console.log(" Maybe next time ")
+// }
+//
+// const username="user";
+// const password ='password';
+//
+// // prompt the user to enter a username and a password
+// const inputedUserName= prompt("please enter username")
+// const inputedPassword = prompt("Please enter a password")
+//
+// // next i want to console.log('You're logged in') if username, inputedUsername && password,  inputedPassowrd match
+//
+// if (username === inputedUserName && password === inputedPassword){
+//     console.log("You're logged in!")
+// } else if (username!== inputedUserName && password!==inputedPassword){
+//     console.log("Incorrect username and password")
+// } else if (username !== inputedUserName){
+//     console.log("Incorrect Username")
+// } else if (password !== inputedPassword){
+//     console.log("incorrect password")
+// } else {
+//     console.log("Unexpected error")
+// }
 
-if (age<16){
-    console.log("You can not drive")
-}
-// same example with else statement
 
-if (age>= 16){
-    console.log("You can drive")
-} else {
-    console.log("grow up")
-}
+// Nest Conditions example
+//
+// if (username===inputedUsername){
+//     if(password=== inputedPassword){
+//         console.log(("logged in"))
+//     }else {
+//         console.log("Incorrect password")
+//     }
+// } else {
+//     console.log("Invalid username")
+// }
+
+// Ternary Operator
+// ? -> represents put "if" condition
+// : -> represent "else" condition
+
+let name="Bob";
+let greeting =(name==="Bob")? ("Hello,"+name ): ("Hello Stranger!");
+console.log(greeting);
+
+let currentCurrentDay=currentDay==='monday'||currentDay==='tuesday'||currentDay==='wednesday'?" I love "+currentDay:currentDay+" is not a validday":
+console.log(currentCurrentDay)
+
+
+
+
+
 
 "use strict";
 
