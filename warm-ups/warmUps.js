b762b9588e0f49ff813d8b4b5f0486b436b2b9ba
oluwaseun anthony <oluwaseun.a.anthony@gmail.com>
2023-10-17 09:51:02 -0500

    completed morning warmup

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -1,9 +1,31 @@
-const capitalizeFirstLetter=(str)=>{
+// const capitalizeFirstLetter=(str)=>{
+//
+//     let capitalizedFirstLetter =  str.substring(0,1).toUpperCase();
+//     let restOfWord = str.substring(1);
+//
+//     return capitalizedFirstLetter + restOfWord;
+// }
+//
+// console.log(capitalizeFirstLetter("testString"))
+//
+// const largestNum=()=>{
+//     numbers= 3,5,7
+// }
 
-    let capitalizedFirstLetter =  str.substring(0,1).toUpperCase();
-    let restOfWord = str.substring(1);
 
-    return capitalizedFirstLetter + restOfWord;
+// using a for loop to find the largest number in an array
+let array = [3, 4, 5, 30, 21, 9];
+let largest = 0;
+
+for (i = 0; i < array.length; i++) {
+    if (array[i] > largest) {
+        largest = array[i];
+    }
+}
+console.log(largest)
+
+function largestNum() {
+    return Math.max(1, 2, 3, 4);
 }
 
-console.log(capitalizeFirstLetter("testString"))
+console.log(largestNum()); // You need to call the function with () to get the result
