2ee4cb475a54f13b2e6070f5b9f8904a69a5dca6
oluwaseun anthony <oluwaseun.a.anthony@gmail.com>
2023-10-19 15:18:18 -0500

    changed some of the code on the break and contiune file

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,22 +1,19 @@
 while (true) {
+    // prompt user and use parse bcz we are looking for a number/integer
     let userInput = parseFloat(prompt("Enter an odd number between 1 and 50"));
-    if (userInput >= 1 && userInput <= 50 && userInput % 2 !== 0) {
+    // verify if it's a odd number that's between 1 and 50
+    if (userInput >= 1 && userInput <= 50 && userInput % 2 !== 1) {
         console.log(`Number Entered: ${userInput}`);
-        console.log("");
 
-        for (let i = 1; i <= 50; i++) {
+        for (let i = 1; i <= 50; i +=2) {
             if (i === userInput) {
-                console.log(`yikes skipping number: ${userInput}`);
+                console.log(`Yikes skipping number: ${userInput}`);
                 continue;
             }
-
-            if (i % 2 !== 0) {
                 console.log(`Here is an odd number: ${i}`);
             }
-        }
-
         break;
-    } else {
-        alert("Invalid input. Please enter a valid odd number between 1 and 50");
+        } else {alert("Invalid input. Please enter a valid odd number between 1 and 50");
     }
+
 }
