94b7b6a464a0448352574def54415e55d3970ff3
oluwaseun anthony <oluwaseun.a.anthony@gmail.com>
2023-10-19 11:11:14 -0500

    created a for loop js file for excises

---
+++ js/for_loops.js
@@ -0,0 +1,8 @@
+function showMultiplicationTable(number) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${number} x ${i} = ${number * i}`);
+    }
+}
+
+// Test the function
+showMultiplicationTable(8);
