5870f8cea6728b5f56c770b31cd965c99ee1cdf4
oluwaseun anthony <oluwaseun.a.anthony@gmail.com>
2023-10-19 14:30:54 -0500

    completed loop exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,2 +1,26 @@
-prompt("Enter an odd number between 1 and 50")
 
+
+while(true) {
+    let userInput = parseFloat(prompt("Enter an odd number between 1 and 50"));
+    if (userInput >= && userInput <= 50 && userInput % 2 !== 0) {
+        console.log(`Number Entered: ${userInput}`);
+        console.log("")
+
+        for (let i = 1; i <= 50; i++) {
+            if (i === userInput) {
+                console.log(`yikes skipping number: ${userInput}`)
+                continue;
+            }
+
+            if (i % 2 !== 0) {
+                console.log(`Here is an odd number:${1}`);
+
+            }
+        }
+
+        break;
+    } else {
+        alert("Invalid input. Please enter a valid odd number between 1 and 50");
+    }
+
+}
