4f8b0bba39f29e43b5cf0cdd646412e8f9d2636d
oluwaseun anthony <oluwaseun.a.anthony@gmail.com>
2023-10-19 09:41:46 -0500

    completed moring warmup excise for while loops

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -14,18 +14,37 @@
 
 
 // using a for loop to find the largest number in an array
-let array = [3, 4, 5, 30, 21, 9];
-let largest = 0;
+// let array = [3, 4, 5, 30, 21, 9];
+// let largest = 0;
+//
+// for (i = 0; i < array.length; i++) {
+//     if (array[i] > largest) {
+//         largest = array[i];
+//     }
+// }
+// console.log(largest)
+//
+// function largestNum() {
+//     return Math.max(1, 2, 3, 4);
+// }
+//
+// console.log(largestNum()); // You need to call the function with () to get the result
 
-for (i = 0; i < array.length; i++) {
-    if (array[i] > largest) {
-        largest = array[i];
+
+let num = 100
+// while (num >= 5) {
+// num-=5;
+// console.log(num);
+// if(num % 2===0){
+//     console.log(`${num} is even`) ;
+// }
+// }
+
+do {
+    num -=5
+    if (num % 2== 0 && num !==0){
+        consoe.log(`${num} is even`);
+    } else{
+        console.log(num);
     }
-}
-console.log(largest)
-
-function largestNum() {
-    return Math.max(1, 2, 3, 4);
-}
-
-console.log(largestNum()); // You need to call the function with () to get the result
+} while (num>105)
