344f5b00f742ea823623a0ab23ee605db67d39fa
oluwaseun anthony <oluwaseun.a.anthony@gmail.com>
2023-11-15 14:05:14 -0600

    completed konami exercise

--- attributes-script.js
+++ attributes-script.js
@@ -17,29 +17,29 @@ document.addEventListener('DOMContentLoaded', function () {
     }, 6000);
 
     // Toggle background color every 2 seconds
-    setInterval(function () {
-        document.getElementById('profile-card').classList.toggle('background-color');
-    }, 2000);
+//     setInterval(function () {
+//         document.getElementById('profile-card').classList.toggle('background-color');
+//     }, 2000);
 });
-
-// Function to toggle dark mode
-function toggleDarkMode() {
-    var body = document.getElementById('toggle-body');
-    document.body.classList.toggle('dark-mode');
-}
-
-// Function to be executed at regular intervals
-function intervalFunction() {
-    toggleDarkMode();
-}
-
-// Set interval to execute the function every 2000 milliseconds (2 seconds)
-// setInterval(intervalFunction, 2000);
-
-// Example: Toggle dark mode when a button is clicked (you can use your own trigger)
-var toggleButton = document.getElementById('toggle-dark-mode-button');
-
-// Add event listener for the button click
-toggleButton.addEventListener('click', function() {
-    toggleDarkMode();
-});
+//
+// // Function to toggle dark mode
+// function toggleDarkMode() {
+//     var body = document.getElementById('toggle-body');
+//     document.body.classList.toggle('dark-mode');
+// }
+//
+// // Function to be executed at regular intervals
+// function intervalFunction() {
+//     toggleDarkMode();
+// }
+//
+// // Set interval to execute the function every 2000 milliseconds (2 seconds)
+// // setInterval(intervalFunction, 2000);
+//
+// // Example: Toggle dark mode when a button is clicked (you can use your own trigger)
+// var toggleButton = document.getElementById('toggle-dark-mode-button');
+//
+// // Add event listener for the button click
+// toggleButton.addEventListener('click', function() {
+//     toggleDarkMode();
+// });
