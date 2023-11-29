ee414f3eb131eeca0fed8ef7ca16cec3f0e333d8
oluwaseun anthony <oluwaseun.a.anthony@gmail.com>
2023-11-29 12:25:38 -0600

    updated the new js file mapbox-geocoder-utils.js new code

---
+++ js/mapbox-geocoder-utils.js
@@ -0,0 +1,52 @@
+"use strict";
+
+/***
+ * geocode is a method to search for coordinates based on a physical address and return
+ * @param {string} search is the address to search for the geocoded coordinates
+ * @param {string} token is your API token for MapBox
+ * @returns {Promise} a promise containing the latitude and longitude as a two element array
+ *
+ * EXAMPLE:
+ *
+ *  geocode("San Antonio", API_TOKEN_HERE).then(function(results) {
+ *      // do something with results
+ *  })
+ *
+ */
+function geocode(search, token) {
+    var baseUrl = 'https://api.mapbox.com';
+    var endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(`${baseUrl}${endPoint}${encodeURIComponent(search)}.json?access_token=${token}`)
+        .then( res => res.json() )
+        // to get all the data from the request, comment out the following three lines...
+        .then( data => data.features[0].center);
+}
+
+
+/***
+ * reverseGeocode is a method to search for a physical address based on inputted coordinates
+ * @param {object} coordinates is an object with properties "lat" and "lng" for latitude and longitude
+ * @param {string} token is your API token for MapBox
+ * @returns {Promise} a promise containing the string of the closest matching location to the coordinates provided
+ *
+ * EXAMPLE:
+ *
+ *  reverseGeocode({lat: 32.77, lng: -96.79}, API_TOKEN_HERE).then(function(results) {
+ *      // do something with results
+ *  })
+ *
+ */
+function reverseGeocode(coordinates, token) {
+    // api url
+    let baseUrl = 'https://api.mapbox.com';
+    // the endpopint when i request lng and lat based on location name
+    let endPoint = '/geocoding/v5/mapbox.places/';
+    // this fetch request takes in url endpoint that will  return the lgn,lat
+    return fetch(`${baseUrl}${endPoint}${coordinates.lng},${coordinates.lat}.json?access_token=${token}`)
+        .then( res => res.json() )
+        // to get all the data from the request, comment out the following three lines...
+        .then( data =>
+            // console.log(data)
+            data.features[0].place_name
+        );
+}
