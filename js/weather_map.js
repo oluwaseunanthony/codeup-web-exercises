ab1c8d0b39449e74409db17128b2c0d70711ba5b
oluwaseun anthony <oluwaseun.a.anthony@gmail.com>
2023-12-04 08:30:09 -0600

    finsih the weather project

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,94 +1,97 @@
-// weather_map_combined.js
+// MAPBOX_TOKEN is specified in the keys.js file
 
-// Add your Mapbox map and geocoding logic here
+// define variables for the marker
+let marker = new mapboxgl.Marker();
+// initialize the map
 mapboxgl.accessToken = MAPBOX_API_KEY;
-const map = new mapboxgl.Map({
+let map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v12',
-    center: [-98.4916, 29.4252],
-    zoom: 10
+    style: 'mapbox://styles/mapbox/navigation-night-v1',
+    zoom: 10,
+    center: [-96.803290, 32.777830]
 });
-
-// Function to add marker and popup for a location
-function addLocationMarker(location, popupContent) {
-    const marker = new mapboxgl.Marker()
-        .setLngLat(location)
-        .addTo(map);
-
-    const popup = new mapboxgl.Popup({ closeOnClick: false, closeButton: false })
-        .setHTML(popupContent)
-        .addTo(map);
-
-    // Open the popup when the marker is clicked
-    marker.setPopup(popup);
-}
-
-function geocodeAndFetchWeather(search, token) {
-    let baseUrl = 'https://api.mapbox.com';
-    let endPoint = '/geocoding/v5/mapbox.places/';
-    fetch(`${baseUrl}${endPoint}${encodeURIComponent(search)}.json?access_token=${token}`)
+// when page loads auto set marker to my home location
+map.on('load', function () {
+    marker.setLngLat([-96.803290, 32.777830]).addTo(map);
+});
+// when page loads auto shows the forecast for 5 days at the location
+fetch('https://api.openweathermap.org/data/2.5/forecast?APPID=' + OPEN_WEATHER_API_KEY + '&q=Dallas,US&units=imperial')
+    .then(res => res.json())
+    .then(data => {
+        // clear the previous weather data
+        document.getElementById('weather').innerHTML = '';
+        // loop over the forecast data and create a card for each day
+        for (let i = 0; i < 40; i += 8) {
+            document.getElementById('weather').insertAdjacentHTML('beforeend', ("<div class='weatherCards'><p class='date'>" + data.list[i].dt_txt.substring(0, 10) + "</p>" +
+                "<p>" + data.list[i].main.temp_min + " °F" + " / " + data.list[i].main.temp_max + " °F" + "</p>" +
+                "<img src='http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png'>" +
+                "<hr class='HR'>" +
+                "<p>Description: " + data.list[i].weather[0].description + "</p>" +
+                "<p>Humidity: " + data.list[i].main.humidity + "%" + "<hr class='HR'>" +
+                "<p>Wind: " + data.list[i].wind.speed + " mph" + "<hr class='HR'>" + "</p>" +
+                "<p>Pressure: " + data.list[i].main.pressure + " Hg" + "</p>"));
+        }
+    });
+// handle the map click event for when click on another location it updates
+map.on('click', function (click) {
+    // When user clicks on the map the searchbox disappears
+    document.getElementById('geocoder').style.display = 'none';
+    // set the marker position to the clicked location
+    marker.setLngLat(click.lngLat).addTo(map);
+    // get the weather data for the clicked location
+    fetch('https://api.openweathermap.org/data/2.5/forecast?APPID=' + OPEN_WEATHER_API_KEY + '&lat=' + click.lngLat.lat + '&lon=' + click.lngLat.lng + '&units=imperial')
         .then(res => res.json())
         .then(data => {
-            const coordinates = data.features[0].center;
-            const popupContent = `<p>${search}</p>`;
-            addLocationMarker(coordinates, popupContent);
-
-            // Fetch weather data for the same coordinates
-            // Example fetch for current weather
-            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates[1]}&lon=${coordinates[0]}&appid=${OPEN_WEATHER_API_KEY}`)
-                .then(response => response.json())
-                .then(weatherData => {
-                    console.log(weatherData)
-                    // Convert temperature from Kelvin to Fahrenheit
-                    const temperatureFahrenheit = (weatherData.main.temp - 273.15) * 9/5 + 32;
-
-                    // Display weather information in console (you can update the UI as needed)
-                    console.log('Weather Data:', weatherData);
-                    // Update the temperature in the UI
-                    updateTemperature(temperatureFahrenheit);
+            // clear the previous weather data
+            document.getElementById('weather').innerHTML = '';
+            // loop over the forecast data and create a card for each day
+            for (let i = 0; i < 40; i += 8) {
+                document.getElementById('weather').insertAdjacentHTML('beforeend', ("<div class='weatherCards'><p class='date'>" + data.list[i].dt_txt.substring(0, 10) + "</p>" +
+                    "<p>" + data.list[i].main.temp_min + " °F" + " / " + data.list[i].main.temp_max + " °F" + "</p>" +
+                    "<img src='http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png'>" +
+                    "<hr class='HR'>" +
+                    "<p>Description: " + data.list[i].weather[0].description + "</p>" +
+                    "<p>Humidity: " + data.list[i].main.humidity + "%" + "<hr class='HR'>" +
+                    "<p>Wind: " + data.list[i].wind.speed + " mph" + "<hr class='HR'>" + "</p>" +
+                    "<p>Pressure: " + data.list[i].main.pressure + " Hg" + "</p>"));
+            }
+        });
+});
+//     Search functionality
+function searchLocation() {
+    // get the value entered in the search bar
+    let location = document.getElementById('searchInput').value;
+    // update the current city in the navbar
+    document.getElementById('curr-city').innerHTML = 'Current City: ' + location;
+    // use Mapbox Geocoding API to get the coordinates of the location
+    fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(location) + '.json?access_token=' + MAPBOX_API_KEY)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            // corresponds to the longitute value of the center of the map
+            let longitude = data.features[0].center[0];
+            // corresponds to the lattitude value of the center of the map
+            let latitude = data.features[0].center[1];
+            // update the map center and marker position to the new search result.
+            map.setCenter([longitude, latitude]);
+            marker.setLngLat([longitude, latitude]);
+            // use OpenWeatherMap API to get the weather data for the location
+            fetch('https://api.openweathermap.org/data/2.5/forecast?APPID=' + OPEN_WEATHER_API_KEY + '&lat=' + latitude + '&lon=' + longitude + '&units=imperial')
+                .then(res => res.json())
+                .then(data => {
+                    // clear the previous weather data
+                    document.getElementById('weather').innerHTML = '';
+                    // loop over the forecast data and create a card for each day
+                    for (let i = 0; i < 40; i += 8) {
+                        document.getElementById('weather').insertAdjacentHTML('beforeend', "<div class='weatherCards'><p class='date'>" + data.list[i].dt_txt.substring(0, 10) + "</p>" +
+                            "<p>" + data.list[i].main.temp_min + " °F" + " / " + data.list[i].main.temp_max + " °F" + "</p>" +
+                            "<img src='http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png'>" +
+                            "<hr class='HR'>" +
+                            "<p>Description: " + data.list[i].weather[0].description + "</p>" +
+                            "<p>Humidity: " + data.list[i].main.humidity + "%" + "<hr class='HR'>" +
+                            "<p>Wind: " + data.list[i].wind.speed + " mph" + "<hr class='HR'>" + "</p>" +
+                            "<p>Pressure: " + data.list[i].main.pressure + " Hg" + "</p>")
+                    }
                 });
-
-
-            // Optionally, you can center the map on the searched location
-            map.setCenter(coordinates);
         });
 }
-
-document.getElementById("sub").addEventListener('click', function () {
-    let currentLocation = geocodeAndFetchWeather(document.getElementById('search').value, MAPBOX_API_KEY)
-});
-
-// Example fetch for current weather
-function updateTemperature(temperature) {
-    const currentWeatherDiv = document.getElementById('current-weather');
-    currentWeatherDiv.innerHTML = `<p>Temperature: ${temperature}°C</p>`;
-}
-
-// Example fetch for 5-day forecast
-fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent('search')}&appid=${OPEN_WEATHER_API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-
-        const forecastDiv = document.getElementById('forecast');
-
-        forecastDiv.innerHTML = ''; // Clear previous forecast data
-        const day = data.list;
-
-
-
-        for (let i = 0; i < day.length; i += 8) {
-            const weather = day[i];
-            const date = new Date(weather.dt * 1000);
-            // Convert temperature from Kelvin to Fahrenheit
-            const temperatureFahrenheit = (weather.main.temp - 273.15) * 9/5 + 32;
-
-            forecastDiv.innerHTML += `
-            <div class="day">
-                <p>Date: ${date.toDateString()}</p>
-                <p>Temperature: ${temperatureFahrenheit}°F</p>
-                <p>Weather: ${weather.weather[0].description}</p>
-            </div>
-        `;
-        }
-    })
-    .catch(error => console.error('Error fetching 5-day forecast:', error));
--- js/weather_map_geocoder.js
+++ js/weather_map_geocoder.js
@@ -1,26 +1,26 @@
-// This file is weather_map_geocoder.js
-
-
-// Add your Mapbox map and geocoding logic here
-
-// Example Mapbox map initialization:
-mapboxgl.accessToken = MAPBOX_API_KEY;
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v12',
-    center: [-98.4916, 29.4252],
-    zoom: 10
-});
-
-// Example marker and popup logic:
-const marker = new mapboxgl.Marker()
-    .setLngLat([-98.4916, 29.4252])
-    .addTo(map);
-
-const popup = new mapboxgl.Popup()
-    .setHTML("<p>Marker Popup Content</p>");
-
-marker.setPopup(popup);
-
-// Add geocoding logic to update the map and forecast based on user input
-// You can use the Mapbox Geocoding API or a library like Mapbox GL Geocoder
+// // This file is weather_map_geocoder.js
+//
+//
+// // Add your Mapbox map and geocoding logic here
+//
+// // Example Mapbox map initialization:
+// mapboxgl.accessToken = MAPBOX_API_KEY;
+// const map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/dark-v12',
+//     center: [-98.4916, 29.4252],
+//     zoom: 10
+// });
+//
+// // Example marker and popup logic:
+// const marker = new mapboxgl.Marker()
+//     .setLngLat([-98.4916, 29.4252])
+//     .addTo(map);
+//
+// const popup = new mapboxgl.Popup()
+//     .setHTML("<p>Marker Popup Content</p>");
+//
+// marker.setPopup(popup);
+//
+// // Add geocoding logic to update the map and forecast based on user input
+// // You can use the Mapbox Geocoding API or a library like Mapbox GL Geocoder
